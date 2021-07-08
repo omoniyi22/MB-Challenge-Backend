6d156008e47f33a09e14bf3f08112c2127fd0bd0
omoniyi22 <omoniyi22@gmail.com>
2021-07-08 07:53:14 +0100

    assessment completed

---
+++ build/precache-manifest.7377bf338d76a9b54aae593d540cc52f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62ed23af25fa5a15bce857c833edefc2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64139ccc9870eb79ff04",
+    "url": "/static/css/2.c2bf6b30.chunk.css"
+  },
+  {
+    "revision": "867895e2d40bd0356f6e",
+    "url": "/static/css/main.d14cf3ac.chunk.css"
+  },
+  {
+    "revision": "64139ccc9870eb79ff04",
+    "url": "/static/js/2.c893bf40.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.c893bf40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "867895e2d40bd0356f6e",
+    "url": "/static/js/main.b061f888.chunk.js"
+  },
+  {
+    "revision": "bccb43675d339c2b2a73",
+    "url": "/static/js/runtime-main.48fe03d4.js"
+  },
+  {
+    "revision": "ef0915561cd01c22e5b497ebb4838ab6",
+    "url": "/static/media/Apercu Pro Bold.ef091556.otf"
+  },
+  {
+    "revision": "42bb1a1ede96c27e83b17f86a54a789d",
+    "url": "/static/media/Apercu Pro Regular.42bb1a1e.otf"
+  },
+  {
+    "revision": "b834fd6cc4a1f44b90b13bd2fee24796",
+    "url": "/static/media/Fenotype - Klik ExtraBold.b834fd6c.otf"
+  },
+  {
+    "revision": "9c71d42b6a840ecfda8fc555040a1c76",
+    "url": "/static/media/Montserrat-Bold.9c71d42b.otf"
+  },
+  {
+    "revision": "92db9a0772b3732e6d686fec3711af42",
+    "url": "/static/media/Montserrat-Regular.92db9a07.otf"
+  },
+  {
+    "revision": "3352653dedd571bbc490c8be132b38cd",
+    "url": "/static/media/Poppins-Light.3352653d.ttf"
+  },
+  {
+    "revision": "f61a4eb27371b7453bf5b12ab3648b9e",
+    "url": "/static/media/Poppins-Medium.f61a4eb2.ttf"
+  },
+  {
+    "revision": "8b6af8e5e8324edfd77af8b3b35d7f9c",
+    "url": "/static/media/Poppins-Regular.8b6af8e5.ttf"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "865b25e449878b6b4d1f9e098ff2f510",
+    "url": "/static/media/Quicksand-Medium.865b25e4.ttf"
+  },
+  {
+    "revision": "216d43ee8707910af457af569eda1dec",
+    "url": "/static/media/Quicksand-Regular.216d43ee.ttf"
+  },
+  {
+    "revision": "3fd02d9d10c0479c7b15fa0cbc268513",
+    "url": "/static/media/Raleway-Heavy.3fd02d9d.ttf"
+  },
+  {
+    "revision": "010aae55eee63f16095bc47f7cffae1b",
+    "url": "/static/media/Raleway-Light.010aae55.ttf"
+  },
+  {
+    "revision": "fa56e8c122bb66dbcb913e416bb54c97",
+    "url": "/static/media/Raleway-Medium.fa56e8c1.ttf"
+  },
+  {
+    "revision": "411ef03a8252675e491bf05a428d56e6",
+    "url": "/static/media/Raleway-SemiBold.411ef03a.ttf"
+  }
+]);
