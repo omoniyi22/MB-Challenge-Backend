734cf1c1a157c0856a181b587f8993f82e6922e7
omoniyi22 <omoniyi22@gmail.com>
2022-08-31 08:16:34 +0100

    Worked on Upload Endpoint

--- utils/index.js
+++ utils/index.js
@@ -23,7 +23,10 @@ module.exports.FileValidation = (records, projectSequence) => {
 
 	let checkIfHeadersAreRight = (sequence && fitness && muts) ? "" : " CSV Header must be Sequence, Fitness and Muts only."
 	let eachRowContains3Column = records.every(data => data.length === 3) ? "" : " Each Rows must have only three columns."
-	
+
+	console.log({projectSequence})
+	projectSequence= "343ei9434h09h9__=wed"
+
 	let recordsHasProjectSequence = new RegExp(projectSequence, "i").test(strigifyRecord) ? "" : " Project Sequence must be included in CSV Sequence Column."
 
 	FinalErroMessage = checkIfHeadersAreRight + eachRowContains3Column + recordsHasProjectSequence
